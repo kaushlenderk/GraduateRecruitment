52f82dd8486fd89ad16165d0f028946358d3b5ed
kaush <kaush@192.168.2.103>
2018-11-01 17:36:42 -0230

    publishing app server

---
+++ GraduateRecruitment/GredApp/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express(); 
+ 
+
+/** site title **/
+app.locals.siteTitle = "Memorial University";
+app.locals.underConstruction = "Coming Soon";
+
+/** static resource file **/
+app.use(express.static("./public"));
+
+/** app setting **/
+app.set("port",process.env.PORT || 3335); 
+app.set("view engine","ejs");
+app.set("views","./views")
+
+/** route setting **/
+app.use(require("./routes/index"))
+//app.use(require("./routes/speakers"))
+  
+
+/** start listener **/
+app.listen(app.get("port"),function(){
+    console.log("Listening on port " + app.get("port"));
+});
+ 
